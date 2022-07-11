21b5d9cea74be976b997b7b62613fd38f19759b3
BestLifeEver <100959834+BestLifeEver@users.noreply.github.com>
2022-07-11 15:34:41 -0400

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -38,8 +38,17 @@ function displayTemperature(response){
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city){
 let apiKey = "b08086bc5dcc87767862f2142592d905";
-let city ="New York"
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
   
-  axios.get(apiUrl).then(displayTemperature);
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSubmit);
