f23047e7adbbc18788bf76311f76f465e979152f
BestLifeEver <100959834+BestLifeEver@users.noreply.github.com>
2022-07-11 13:57:22 -0400

    Added relevant icon to API

--- src/app.js
+++ src/app.js
@@ -34,9 +34,12 @@ function displayTemperature(response){
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt* 1000);
+  let iconElement =document.querySelector("#icon");
+  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "b08086bc5dcc87767862f2142592d905";
-let city ="Paris"
+let city ="New York"
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   
   axios.get(apiUrl).then(displayTemperature);
