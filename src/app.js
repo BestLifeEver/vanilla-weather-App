cf2c2992af7a79e059342e83ba80d93eba832ae8
BestLifeEver <100959834+BestLifeEver@users.noreply.github.com>
2022-07-12 18:45:43 -0400

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -20,32 +20,49 @@ function formatDate(timestamp){
   let day = days[date.getDay()];
   return `${day} ${hours}: ${minutes}`;
 }
+function formatDay(timestamp){
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sun",
+    "Mon",
+    "Teu",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat"
+  ];
+  return days[day];
+}
 function displayForecast(response){
   let forecast =response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   
-  forecast.forEach(function(forecastDay){
+  forecast.forEach(function(forecastDay,  index){
+    if (index < 6){
 forecastHTML =
     forecastHTML +
     ` <div class="col-2">
         <div class="weather-forecast-date">
-        ${forecastDay.dt}
+        ${formatDay(forecastDay.dt)}
         </div>
+        
         <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
         alt=""
         width="42"
         />
 <div class="weather-forecast-temperature">
   <span class="weather-forecast-temperature-max">
-    ${forecastDay.temp.max}°
+    ${Math.round(forecastDay.temp.max)}°
     <span class="weather-forecast-temperature-min">
-${forecastDay.temp.min} °</span>
+${Math.round(forecastDay.temp.min)} °</span>
   
         </div>
       </div>
     `;
+  }
 });
   forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML= forecastHTML;
