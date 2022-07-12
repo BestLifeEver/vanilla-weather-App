338cd67776a5ea964becff13b475b469919417cd
BestLifeEver <100959834+BestLifeEver@users.noreply.github.com>
2022-07-11 21:59:41 -0400

    injected large block of html into js

--- src/app.js
+++ src/app.js
@@ -20,7 +20,35 @@ function formatDate(timestamp){
   let day = days[date.getDay()];
   return `${day} ${hours}: ${minutes}`;
 }
-
+function displayForecast(){
+  let forecastElement = document.querySelector("#forecast");
+let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = `<div class="row">`;
+  
+  days.forEach(function(day){
+forecastHTML =
+    forecastHTML +
+    ` <div class="col-2">
+        <div class="weather-forecast-date">
+        ${day}
+        </div>
+        <img src="https://openweathermap.org/img/wn/50d@2x.png"
+        alt=""
+        width="42"
+        />
+<div class="weather-forecast-temperature">
+  <span class="weather-forecast-temperature-max">
+    18°  </span>
+    <span class="weather-forecast-temperature-min">
+12° </span>
+  
+        </div>
+      </div>
+    `;
+});
+  forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML= forecastHTML;
+}
 function displayTemperature(response){
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
@@ -67,6 +95,8 @@ function displayCelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
+
+
   let form = document.querySelector("#search-form");
   form.addEventListener("submit", handleSubmit);
   let fahrenheitLink = document.querySelector("#fahrenheit-link");
@@ -76,3 +106,4 @@ let celsiusTemperature = null;
   celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
   search("New York")
+  displayForecast();
