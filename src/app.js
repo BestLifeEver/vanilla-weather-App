0c69ff9308e15934f4a4737fb28a1c53dcf54537
BestLifeEver <100959834+BestLifeEver@users.noreply.github.com>
2022-07-12 08:02:51 -0400

    modified forecast API with realtime data

--- src/app.js
+++ src/app.js
@@ -20,27 +20,28 @@ function formatDate(timestamp){
   let day = days[date.getDay()];
   return `${day} ${hours}: ${minutes}`;
 }
-function displayForecast(){
+function displayForecast(response){
+  let forecast =response.data.daily;
   let forecastElement = document.querySelector("#forecast");
-let days = ["Thu", "Fri", "Sat", "Sun"];
+
   let forecastHTML = `<div class="row">`;
   
-  days.forEach(function(day){
+  forecast.forEach(function(forecastDay){
 forecastHTML =
     forecastHTML +
     ` <div class="col-2">
         <div class="weather-forecast-date">
-        ${day}
+        ${forecastDay.dt}
         </div>
-        <img src="https://openweathermap.org/img/wn/50d@2x.png"
+        <img src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
         alt=""
         width="42"
         />
 <div class="weather-forecast-temperature">
   <span class="weather-forecast-temperature-max">
-    18°  </span>
+    ${forecastDay.temp.max}°
     <span class="weather-forecast-temperature-min">
-12° </span>
+${forecastDay.temp.min} °</span>
   
         </div>
       </div>
